8f2eae3e915d4d86a8bddd9876fa19a16057e0db
Kauã Onorato <kauaonorato.dev@gmail.com>
2024-06-11 07:28:13 -0400

    fix: fixing variable user

--- order/models/order.py
+++ order/models/order.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from product.models import Product
 
 class Order(models.Model):
     product = models.ManyToManyField(Product, blank=False)
-    user = models.ForeingKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
