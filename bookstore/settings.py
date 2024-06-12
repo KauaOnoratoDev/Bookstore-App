d291d6f3c7327dac147651593e715afc35c1245d
Kauã Onorato <kauaonorato.dev@gmail.com>
2024-06-12 07:27:52 -0400

    build: installing django-extensions and degub-toolbar

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
+    "debug_toolbar",
     "order",
     "product",
 ]
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = "bookstore.urls"
