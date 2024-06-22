e5929e6afacd6de09ba52fcd6734e4b49e138ddf
Kauã Onorato <kauaonorato.dev@gmail.com>
2024-06-21 22:05:56 -0400

    feat: add auto update

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -13,22 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.conf import settings
 import debug_toolbar
+from bookstore import views
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
-    path("api-token-auth/", obtain_auth_token, name='api_token_auth'),
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
